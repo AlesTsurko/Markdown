3262bdfc77ed57c10a1d101a22654c804ccc9095
Ales Tsurko <ales.tsurko@gmail.com>
2018-01-14 14:45:40 +0300

    use MKD_TABSTOP flag

--- source/IoMarkdown.c
+++ source/IoMarkdown.c
@@ -89,7 +89,7 @@ IoObject *IoMarkdown_toHTML(IoMarkdown *self, IoObject *locals, IoMessage *m)
     //doc Markdown toHTML(string) Gets markdown string as input and returns generated HTML.
     IoSeq *input = IoMessage_locals_seqArgAt_(m, locals, 0);
     char *html = 0;
-    int flags = MKD_NOPANTS|MKD_FENCEDCODE|MKD_GITHUBTAGS|MKD_URLENCODEDANCHOR;
+    int flags = MKD_NOPANTS|MKD_FENCEDCODE|MKD_GITHUBTAGS|MKD_URLENCODEDANCHOR|MKD_TABSTOP;
     MMIOT *document = 0;
     IoSeq *outputSequence = IoSeq_new(IOSTATE);
 
