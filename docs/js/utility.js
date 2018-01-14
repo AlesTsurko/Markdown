350a7617b612ee9e1463739deb6b1bae437cb5ee
Ales Tsurko <ales.tsurko@gmail.com>
2018-01-14 14:46:49 +0300

    remove docs

--- docs/js/utility.js
+++
@@ -1,5 +0,0 @@
-// updates address bar with link address including anchor links
-// without reloading the page
-function updateAddressWithAnchorLink(obj) {
-    history.pushState({}, "", obj.href);
-}
